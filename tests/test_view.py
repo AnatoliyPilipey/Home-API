7c0b08c75a063abe282bf86b45201873406b31d7
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-12-12 22:14:33 +0200

    Add more test

--- tests/test_view.py
+++ tests/test_view.py
@@ -5,6 +5,7 @@ from wood.models import Task, Tags
 
 TASK_URL = reverse("wood:task-list")
 TAGS_URL = reverse("wood:tags-list")
+STATUS_URL = reverse("wood:task-status", args=[1, 1])
 
 
 class PublicTests(TestCase):
@@ -41,3 +42,45 @@ class PublicTests(TestCase):
             list(response.context["task_list"]),
             list(task)
         )
+
+    def test_task_str_model(self):
+        task_test = Task.objects.create(
+            content="test2",
+            status=True,
+        )
+
+        self.assertEqual(
+            str(task_test),
+            task_test.content
+        )
+
+    def test_create_task(self):
+        content = "test2",
+        tags_test = Tags.objects.create(
+            name="test1",
+        )
+        task = Task.objects.create(
+            content=content,
+            status=True,
+        )
+        task.tags.add(tags_test)
+
+        self.assertEqual(task.content, content)
+        self.assertEqual(task.status, True)
+
+    def test_task_cheng_status(self):
+        tags_test = Tags.objects.create(
+            name="test1",
+        )
+        task = Task.objects.create(
+            content="test2",
+            status=False,
+        )
+        task.tags.add(tags_test)
+        response = self.client.get(STATUS_URL)
+        self.assertEqual(
+            response.context["task_list"].get(pk=1).status,
+            True
+        )
+
+
